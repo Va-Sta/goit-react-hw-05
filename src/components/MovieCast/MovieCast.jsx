e0b4676a5b767b5d6ba348ca694cd407b2a868f2
Falka <valentyna.stashenko@gmail.com>
2025-04-20 16:37:51 +0300

    dark theme fix

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -3,14 +3,12 @@ import { useParams } from "react-router-dom";
 import { getMovieCredits } from "../../services/api_tmdb.js";
 import { ConfigContext } from "../../context/ConfigContext";
 import noimage from "../../../public/no_actor_image.png";
-import myimage from "../../../public/myimage.png";
 import css from "./MovieCast.module.css";
 
 const MovieCast = () => {
   const { movieId } = useParams();
   const config = useContext(ConfigContext);
   const [movieCredits, setMovieCredits] = useState({});
-  const [flipped, setFlipped] = useState({});
 
   useEffect(() => {
     getMovieCredits(movieId)
@@ -22,13 +20,6 @@ const MovieCast = () => {
       });
   }, [movieId]);
 
-  const handleFlip = (id) => {
-    setFlipped((prevState) => ({
-      ...prevState,
-      [id]: !prevState[id],
-    }));
-  };
-
   if (!config) {
     return <p>Loading configuration...</p>;
   }
@@ -38,27 +29,15 @@ const MovieCast = () => {
       {movieCredits.cast && movieCredits.cast.length > 0 ? (
         movieCredits.cast.map((item) => (
           <li key={item.id} className={css.castItem}>
-            <div
-              className={`${css.castCard} ${
-                flipped[item.id] ? css.flipped : ""
-              }`}
-              onClick={() => handleFlip(item.id)}
-            >
-              <div className={css.front}>
-                <img
-                  className={css.castImage}
-                  src={
-                    item.profile_path
-                      ? config.images.base_url + "w185" + item.profile_path
-                      : noimage
-                  }
-                  alt={item.name}
-                />
-              </div>
-              <div className={css.back}>
-                <img className={css.castImage} src={myimage} alt="Actor back" />
-              </div>
-            </div>
+            <img
+              className={css.castImage}
+              src={
+                item.profile_path
+                  ? config.images.base_url + "w185" + item.profile_path
+                  : noimage
+              }
+              alt={item.name}
+            />
             <div className={css.castInfo}>
               <div className={css.castName}>{item.name}</div>
               <div className={css.castCharacter}>as {item.character}</div>
